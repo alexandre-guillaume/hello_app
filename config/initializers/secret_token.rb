eae932e638165fe781ae034280d0c3585b8115f4
alex <alexandrelepettidu49@hotmail.fr>
2015-02-01 17:30:32 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HellowApp::Application.config.secret_key_base = '62ce17ea471d252f8d43f24946b1ec0b21a299d51574108ec825a3e2eaa8741497baa8619510a073dac6861de9652f8bccdaaf9ff3a77406f73f33f746d8d0a2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HellowApp::Application.config.session_store :cookie_store, key: '_hellow_app_session'
